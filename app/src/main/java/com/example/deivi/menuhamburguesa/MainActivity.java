7c81e8827e80b878d433734262ffba05d7596671
David Hernández Mingorance <deividhermin@gmail.com>
2016-11-29 17:14:42 +0100

    El progress bar indica el numero de circulos/rectangulos/ovalos que se van a dibujar. El boton relleno indica si el dibujo estara relleno o no. Pulsar  el canvas indica donde estara el centro desde el que empezara a crear el dibujo

--- app/src/main/java/com/example/deivi/menuhamburguesa/MainActivity.java
+++ app/src/main/java/com/example/deivi/menuhamburguesa/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.TextViewCompat;
 import android.text.Layout;
+import android.view.MotionEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -23,14 +24,22 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import static com.example.deivi.menuhamburguesa.R.id.textView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     ProgressBar bar;
-    TextView tv;
+    public TextView tv;
+    public float cox, coy;
+    Switch sw;
     int i=0;
     int opc;
     //Lienzo fondo;
@@ -52,9 +61,10 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        sw = (Switch)findViewById(R.id.switch1);
         tv = (TextView)findViewById(R.id.lbProgress);
         bar = (ProgressBar)findViewById(R.id.progressbar);
-        bar.setMax(100);
+        bar.setMax(50);
 
         //layout1 = (LinearLayout)findViewById(R.id.layout);
     }
@@ -92,10 +102,10 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
 
-        LinearLayout layout1 = (LinearLayout)findViewById(R.id.layout);
+
         if (id == R.id.nav_circunferencias) {
             opc=1;
-            Toast.makeText(MainActivity.this, "opc1", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(MainActivity.this, "opc1", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_rectangulos) {
             opc=2;
         } else if (id == R.id.nav_ovalos) {
@@ -108,8 +118,20 @@ public class MainActivity extends AppCompatActivity
             hilo();
         }
 
-        Lienzo fondo = new Lienzo(this);
+        RelativeLayout layout1 = (RelativeLayout)findViewById(R.id.layout);
+        layout1.setOnTouchListener( new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch( View arg0, MotionEvent arg1 ) {
+                cox=arg1.getX();
+                coy=arg1.getY();
+                Toast.makeText(getApplication(), "X="+cox+" Y="+coy, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        Lienzo fondo = new Lienzo(this, tv, sw);
         layout1.addView(fondo);
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
@@ -119,7 +141,7 @@ public class MainActivity extends AppCompatActivity
 
     public void pulsar(){
         bar.setVisibility(View.VISIBLE);
-        if (i>100)
+        if (i>50)
             i=0;
         bar.setProgress(i++);
         tv.setText(""+i);
@@ -133,7 +155,11 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-
+    public void addFive(View v){
+        for(int i=0; i<5; i++){
+            pulsar();
+        }
+    }
 
     public void hilo(){
         AsyncTaskCargaDatos as=new AsyncTaskCargaDatos();
@@ -147,13 +173,13 @@ public class MainActivity extends AppCompatActivity
         @Override
         protected Boolean doInBackground(Void... params) {
 
-            for(int i=1; i<=10; i++) {
+            for(int i=1; i<=50; i++) {
                 //tareaLarga();
                 try {
-                    Thread.sleep(1000);}
+                    Thread.sleep(100);}
                 catch (InterruptedException e) {}
 
-                publishProgress(i*10);
+                publishProgress(i*1);
 
                 if(isCancelled())
                     break;
@@ -171,7 +197,7 @@ public class MainActivity extends AppCompatActivity
 
         @Override
         protected void onPreExecute() {
-            bar.setMax(100);
+            bar.setMax(50);
             bar.setProgress(0);
         }
 
@@ -191,21 +217,27 @@ public class MainActivity extends AppCompatActivity
 
     class Lienzo extends View {
 
-        public Lienzo(Context context) {
+        public Lienzo(Context context, TextView tv, Switch sw) {
             super(context);
         }
 
+        //TextView tv;
         protected void onDraw(Canvas canvas) {
-            Toast.makeText(MainActivity.this, "Entra onDraw", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(MainActivity.this, "Entra onDraw", Toast.LENGTH_SHORT).show();
             int ancho = canvas.getWidth();
             int alto = canvas.getHeight();
+
+            //tv = (TextView)findViewById(R.id.lbProgress);
 
             Paint pincel1 = new Paint();
 
             limpiar(canvas, pincel1, ancho, alto);
 
-            pincel1.setARGB( 255, 255, 0, 0);
-            pincel1.setStyle(Paint.Style.STROKE);
+            pincel1.setARGB(255, 255, 0, 0);
+            if(sw.isChecked())
+                pincel1.setStyle(Paint.Style.FILL);
+            else
+                pincel1.setStyle(Paint.Style.STROKE);
 
             switch (opc){
                 case 1: dibujaCirculo(canvas, pincel1, ancho, alto);
@@ -221,28 +253,28 @@ public class MainActivity extends AppCompatActivity
             pincel1.setColor(Color.WHITE);
             pincel1.setStyle(Paint.Style.FILL);
             canvas.drawRect(0, 0, ancho, alto, pincel1);
-            Toast.makeText(MainActivity.this, "Limpiado", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(MainActivity.this, "Limpiado", Toast.LENGTH_SHORT).show();
         }
 
         public void dibujaCirculo(Canvas canvas, Paint pincel1, int ancho, int alto){
-            for (int f = 0; f < 10; f++) {
-                canvas.drawCircle(ancho / 6, alto / 9, f * 15, pincel1);
+            for (int f = 0; f < Integer.parseInt(tv.getText().toString()); f++) {
+                canvas.drawCircle(cox, coy, f * 20, pincel1);
             }
-            Toast.makeText(MainActivity.this, "Circulo dibujado", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(MainActivity.this, "Circulo dibujado", Toast.LENGTH_SHORT).show();
         }
 
         public void dibujaRectangulo(Canvas canvas, Paint pincel1, int ancho, int alto){
-            for (int f = 0; f < 40; f++) {
-                canvas.drawRect(f*15, f*15, f*15+300, f*15+300, pincel1);
+            for (int f = 0; f < Integer.parseInt(tv.getText().toString()); f++) {
+                canvas.drawRect(cox, coy, f*15+300, f*15+300, pincel1);
             }
-            Toast.makeText(MainActivity.this, "Rectangulo dibujado", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(MainActivity.this, "Rectangulo dibujado", Toast.LENGTH_SHORT).show();
         }
 
         public void dibujaOvalo(Canvas canvas, Paint pincel1, int ancho, int alto){
-            for (int f = 0; f < 10; f++) {
-                canvas.drawOval(ancho / 3, alto / 4, f * 15, f * 15, pincel1);
+            for (int f = 0; f < Integer.parseInt(tv.getText().toString()); f++) {
+                canvas.drawOval(cox, coy / 4, f * 30, f * 45, pincel1);
             }
-            Toast.makeText(MainActivity.this, "Ovalo dibujado", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(MainActivity.this, "Ovalo dibujado", Toast.LENGTH_SHORT).show(); Integer.parseInt(tv.getText().toString())
         }
     }
 }
